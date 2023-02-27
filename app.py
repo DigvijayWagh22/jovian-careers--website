897a88a4d408e1200bf32e0cf0bff5ec58e7bfa7
Digvijay <126435074+DigvijayWagh22@users.noreply.github.com>
2023-02-27 05:40:32 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 @app.route("/")
 def hello_world():
-   return "Hello , Jovian !"
+   return render_template('home.html')
 
 print("Hello world")
 if __name__=="__main__":
